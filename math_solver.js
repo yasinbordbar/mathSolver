073e964ef6d705624ef29f3db61d7954f06a26b1
Yasin Bordbar <yasinbordbar@outlook.com>
2021-10-07 21:39:48 +0330

    Logic

---
+++ math_solver.js
@@ -0,0 +1,52 @@
+const PARENTHESIS_REGEX = /\((?<equation>[^\(\)]*)\)/;
+
+const MULTIPLY_DIVIDE_REGEX =
+  /(?<operand1>\S+)\s*(?<operation>[\/\*])\s*(?<operand3>\S+)/;
+
+const ADD_SUBTRACT_REGEX =
+  /(?<operand1>\S+)\s*(?<operation>(?<!e)[\-\+])\s*(?<operand3>\S+)/;
+
+const EXPONENT_REGEX = /(?<operand1>\S+)\s*(?<operation>\^)\s*(?<operand3>\S+)/;
+
+function parse(equation) {
+  if (equation.match(PARENTHESIS_REGEX)) {
+    const subEquation = equation.match(PARENTHESIS_REGEX).groups.equation;
+    const result = parse(subEquation);
+    const newEquation = equation.replace(PARENTHESIS_REGEX, result);
+    return parse(newEquation);
+  } else if (equation.match(EXPONENT_REGEX)) {
+    const result = handleMath(equation.match(EXPONENT_REGEX).groups);
+    const newEquation = equation.replace(EXPONENT_REGEX, result);
+    return parse(newEquation);
+  } else if (equation.match(MULTIPLY_DIVIDE_REGEX)) {
+    const result = handleMath(equation.match(MULTIPLY_DIVIDE_REGEX).groups);
+    const newEquation = equation.replace(MULTIPLY_DIVIDE_REGEX, result);
+    return parse(newEquation);
+  } else if (equation.match(ADD_SUBTRACT_REGEX)) {
+    const result = handleMath(equation.match(ADD_SUBTRACT_REGEX).groups);
+    const newEquation = equation.replace(ADD_SUBTRACT_REGEX, result);
+    return parse(newEquation);
+  } else {
+    return parseFloat(equation);
+  }
+}
+
+function handleMath({ operand1, operation, operand3 }) {
+  const number1 = parseFloat(operand1);
+  const number2 = parseFloat(operand3);
+
+  switch (operation) {
+    case "*":
+      return number1 * number2;
+    case "/":
+      return number1 / number2;
+    case "+":
+      return number1 + number2;
+    case "-":
+      return number1 - number2;
+    case "^":
+      return number1 ** number2;
+  }
+}
+
+console.log(parse("127")); //7
